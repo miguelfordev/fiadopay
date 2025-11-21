3b59fd581559796de98f7814f0190f1866a03026
alefvilav <alef.silva@ucsal.edu.br>
2025-11-21 13:42:15 -0300

    Revert "Atualizacao Alef"
    This reverts commit f629e9c50b50756556c85d37f93cfdab71bb298f.

--- src/main/java/edu/ucsal/fiadopay/annotations/PaymentHandler.java
+++
@@ -1,9 +0,0 @@
-package edu.ucsal.fiadopay.annotations;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface PaymentHandler {
-    String value();
-}
